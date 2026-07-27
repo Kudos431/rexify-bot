0fab5d6f218716a268c58727ac6b78db5c8dfe1e
shola1236 <akinosifadeshola@gmail.com>
2026-07-27 11:44:34 +0100

    Delete .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Directs Puppeteer to store Chrome inside the project folder
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
